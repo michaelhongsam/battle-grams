b8b2965b9b17173c8b37b89a7427e1df2f3cc525
Kyle Yee <20980584+ShifuYee@users.noreply.github.com>
2018-03-09 13:14:09 -0500

    feat(client): created a test for tile and refactored bank, tileSlot, and tile

--- src/components/tileSlot.js
+++ src/components/tileSlot.js
@@ -1,44 +1,48 @@
 import React, { Component } from 'react';
-import Tile from './tile';
+// import Tile from './tile';
 
 import PropTypes from 'prop-types';
 import { DropTarget } from 'react-dnd';
 import ItemTypes from './ItemTypes';
 
 const tileTarget = {
-  drop() {
-    return { name: 'tileSlot' }
+  drop(props) {
+    return {
+      name: 'tileSlot',
+      cellId: props.cellId
+     };
   },
-}
+};
 
 function collect (connect, monitor) {
   return {
     connectDropTarget: connect.dropTarget(),
     isOver: monitor.isOver(),
     canDrop: monitor.canDrop(),
-  }
+  };
 }
 
 const propTypes = {
   connectDropTarget: PropTypes.func.isRequired,
   isOver: PropTypes.bool.isRequired,
   canDrop: PropTypes.bool.isRequired,
-}
+};
 
 class TileSlot extends Component {
-  
+
   render() {
-    const { canDrop, isOver, connectDropTarget, letter } = this.props
+    const { canDrop, isOver, connectDropTarget } = this.props;
     // const isActive = canDrop && isOver
-    
+
     return connectDropTarget(
       <div className = "tile-slot">
-        <Tile value={letter} />
+        {this.props.children}
       </div>
-    )
+
+    );
   }
 }
 
 TileSlot.propTypes = propTypes;
 
-export default DropTarget(ItemTypes.TILE, tileTarget, collect)(TileSlot)
+export default DropTarget(ItemTypes.TILE, tileTarget, collect)(TileSlot);
